01ff6108ee38540960bc570535df7573511894bb
Ben Castilla <ben@castilla.us>
2024-01-31 05:56:44 -0800

    Minting NFT and tests

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -5,10 +5,16 @@ import "./ERC721Enumerable.sol";
 import "./Ownable.sol";
 
 contract NFT is ERC721Enumerable, Ownable {
+    using Strings for uint256;
+
+    string public baseURI;
+    string public baseExtension = ".json";
     uint256 public cost;
     uint256 public maxSupply;
     uint256 public allowMintingOn;
-    string public baseURI;
+
+
+    event Mint(uint256 amount, address minter);
 
     constructor(
         string memory _name, 
@@ -17,6 +23,7 @@ contract NFT is ERC721Enumerable, Ownable {
         uint256 _maxSupply,
         uint256 _allowMintingOn,
         string memory _baseURI
+
 
     ) ERC721(_name, _symbol) {
         cost = _cost;
@@ -29,20 +36,47 @@ contract NFT is ERC721Enumerable, Ownable {
         // Only allow minting after a specified time
         require(block.timestamp >= allowMintingOn);
 
+        // Must mint at least 1 token
+        require(_mintAmount > 0);
 
         // Require enough payment
         require(msg.value >= cost * _mintAmount);
 
-        // Create a token
+
         uint256 supply = totalSupply();
 
+        // Do not them mint more tokens than available
+        require(supply + _mintAmount <= maxSupply);
 
-
+        // Create a token
         for(uint256 i = 1; i <= _mintAmount; i++ ) {
             _safeMint(msg.sender, supply + i);
         }
 
+        // Emit event
+        emit Mint(_mintAmount, msg.sender);
         
+    }
+    // Return metadata IPFS url
+    // EG: 'ipfs://QmQPEMsfd1tJnqYPbnTQCjoa8vczfsV1FmqZWgRdNQ7z3g/1.json'
+    function tokenURI(uint256 _tokenId) 
+        public 
+        view 
+        virtual 
+        override 
+        returns(string memory)
+    {
+        require(_exists(_tokenId), 'token does not exist');
+        return(string(abi.encodePacked(baseURI, _tokenId.toString(), baseExtension)));
+    }
+
+    function walletOfOwner(address _owner) public view returns(uint256[] memory) {
+        uint256 ownerTokenCount = balanceOf(_owner);
+        uint256[] memory tokenIds = new uint256[](ownerTokenCount);
+        for(uint256 i; i < ownerTokenCount; i++) {
+            tokenIds[i] = tokenOfOwnerByIndex(_owner, i);
+        }
+        return tokenIds;
     }
 
 }
